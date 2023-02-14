f3902e50a6b753ccfde050b5bcb137ad58c45465
Wenjun Mao <mkof616@gmail.com>
2023-02-14 01:49:02 +0000

    Initial commit

---
+++ siads699/example/views.py
@@ -0,0 +1,40 @@
+from django.http import HttpResponse
+from django.views import View
+from django.shortcuts import render
+import openai
+
+openai.api_key = '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'
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the siads699 index.")
+
+
+class InitialView(View):
+    template_name = "example/initial.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        question = request.POST['question']
+        try:
+            response = openai.Completion.create(
+                model='davinci:ft-personal-2023-01-15-03-48-41',
+                # prompt = question,
+                #design the prompt to take in the question and the context
+                # prompt = 'pretend to be a data analyst and read the df.' + question + '?',
+                prompt='pretend to be a data analyst.' + question + '?',
+                max_tokens=50,
+                # n=1,
+                # stop=None,
+                # stop after the first sentence
+                stop=['Sentence'],
+                temperature=0.1,
+                # frequency_penalty = 0.1
+                # presence_penalty = 0.1
+            )
+        except:
+            response = "well...,..."
+        ctx = {'response': response["choices"][0]["text"]}
+        return render(request, self.template_name, ctx)
