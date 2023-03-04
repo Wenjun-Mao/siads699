d8624127146be4c203eb7e0020503022ced436b3
Wenjun Mao <mkof616@gmail.com>
2023-03-03 23:09:58 -0500

    STATIC_URL = 'static/'

--- siads699/siads699/settings.py
+++ siads699/siads699/settings.py
@@ -23,6 +23,7 @@ APP_NAME = 'MADSTeam_Samples'   # Add
 
 # Used for static files
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = 'static/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
