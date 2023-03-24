dcd57c23117e335f6e24f98c1563ed7ec895e53b
Wenjun Mao <mkof616@gmail.com>
2023-03-24 16:53:44 -0400

    update e3 url

--- siads699/example3/urls.py
+++ siads699/example3/urls.py
@@ -10,7 +10,7 @@ app_name = 'example3'
 # ]
 
 urlpatterns = [
-    path('', views00.AskQuestionView.as_view(), name='ask_question'),
+    path('example3', views00.AskQuestionView.as_view(), name='ask_question'),
     path('question/int:question_id/', views00.QuestionDetailView.as_view(), name='question_detail'),
     path('question/int:question_id/accept/', views00.accept_answer, name='accept_answer'),
     path('question/int:question_id/retry/', views00.retry_second_step, name='retry_second_step'),
