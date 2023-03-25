4667e4df2bc1f43b88f467c8d6360177f04fd664
Wenjun Mao <mkof616@gmail.com>
2023-03-25 16:38:18 -0400

    dotenv_path

--- siads699/siads699/settings.py
+++ siads699/siads699/settings.py
@@ -16,7 +16,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 import openai
 
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
+load_dotenv(dotenv_path)
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 
